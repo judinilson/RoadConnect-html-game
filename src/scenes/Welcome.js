7eef98488cff0f87f656bc4215474fd2dcb0f42f
judinilson monchacha <judinilson0000@gmail.com>
2022-08-11 18:35:26 +0400

    implemented play button

--- src/scenes/Welcome.js
+++ src/scenes/Welcome.js
@@ -9,6 +9,11 @@ export default class Welcome extends Phaser.Scene {
       "assets/fonts/nonstopfont.png",
       "assets/fonts/nonstopfont.xml"
     );
+    this.load.bitmapFont(
+      "customFont",
+      "assets/fonts/a2203.png",
+      "assets/fonts/a2203.xml"
+    );
   }
 
   create() {
@@ -17,16 +22,24 @@ export default class Welcome extends Phaser.Scene {
     const connectText = this.add
       .bitmapText(230, 100, "nonstopFont", "", 30)
       .setOrigin(0.5)
-      .setCenterAlign()
-      .setInteractive();
+      .setCenterAlign();
 
     const roadText = this.add
       .bitmapText(230, 140, "nonstopFont", "", 30)
       .setOrigin(0.5)
+      .setCenterAlign();
+
+    const playText = this.add
+      .bitmapText(230, 500, "customFont", "", 30)
+      .setOrigin(0.5)
       .setCenterAlign()
-      .setInteractive();
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.scene.start("Menu");
+      });
 
     connectText.setText("Connect");
     roadText.setText("Road");
+    playText.setText("Play");
   }
 }
